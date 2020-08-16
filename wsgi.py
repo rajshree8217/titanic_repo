2fe89fc236a0a34781c8d86f13d8520cf9aa76a5
Rajshree Ramki <rajshree@awardsolutions.com>
2020-08-15 22:09:23 -0500

    added global median_imputer to make_pred

--- wsgi.py
+++ wsgi.py
@@ -500,6 +500,8 @@ def predform():
 @application.route("/make_pred", methods=['POST'])
 def make_pred():
     
+    global MEDIAN_IMPUTER
+    
     file_obj = request.files.get('newdata')
     print("Type of the file is :", type(file_obj))
     name = file_obj.filename
